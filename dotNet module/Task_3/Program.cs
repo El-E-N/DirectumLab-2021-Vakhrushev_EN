8f9768d3f6f1f27e30f46af455170b9451d74046
El-E-N <egor.v1309@gmail.com>
2021-03-05 01:06:01 +0400

    Реализовано, но не красиво

--- dotNet module/Task_3/Program.cs
+++ dotNet module/Task_3/Program.cs
@@ -2,51 +2,31 @@
 
 namespace Task_3
 {
+    /// <summary>
+    /// Координаты точки на плоскости
+    /// </summary>
     public struct Point
     {
         public double x;
         public double y;
-
-        /*public double this[int c]
-        {
-            get
-            {
-                if (c == 0)
-                    return this.x;
-                return this.y;
-            }
-            set
-            {
-                if (c == 0)
-                    this.x = value;
-                else
-                    this.y = value;
-            }
-        }*/
     }
 
+    /// <summary>
+    /// Фигура
+    /// </summary>
     public class Shape
     {
-        private Point[] coordinates;
-
         public Shape(int count)
         {
-            coordinates = new Point[count];
+            this.Coordinates = new Point[count];
         }
 
         public Shape(Point[] coords)
         {
-            coordinates = coords;
+            this.Coordinates = coords;
         }
 
-        public Point[] Coordinates
-        {
-            get => this.coordinates;
-            set 
-            {
-                this.coordinates = value;
-            }
-        }
+        public Point[] Coordinates { get; set; }
     }
 
     /// <summary>
@@ -54,34 +34,28 @@ namespace Task_3
     /// </summary>
     public class Circle : Shape
     {
-        private double radius = 0;
-
-        public double R
-        {
-            get => this.radius;
-            set
-            {
-                this.radius = value;
-            }
-        }
+        public double Radius { get; set; }
 
         public Circle() 
             : base(1)
         {
-            this.radius = 0;
+            this.Radius = 0;
         }
 
         public Circle(Point[] coords, double r) 
             : base(coords)
         {
-            this.radius = r;
+            this.Radius = r;
         }
 
-        public virtual double Square() => 0;
+        public virtual double Area() => 0; // площадь
 
-        public double Length() => 2 * Math.PI * this.radius;
+        public double Length() => 2 * Math.PI * this.Radius; // длина окружности
     }
 
+    /// <summary>
+    /// Круг
+    /// </summary>
     public class Round : Circle
     {
         public Round()
@@ -89,53 +63,62 @@ namespace Task_3
         public Round(Point[] coords, double r)
             : base(coords, r) { }
 
-        public override double Square() => Math.PI * this.R * this.R;
+        public override double Area() => Math.PI * this.Radius * this.Radius;
     }
 
+    /// <summary>
+    /// Кольцо
+    /// </summary>
     public class Ring : Shape
     {
-        private double radiusMin;
-        private double radiusMax;
-
         public Ring()
             : base(1)
         {
-            (radiusMin, radiusMax) = (0, 0);
+            (RadiusMin, RadiusMax) = (0, 0);
         }
 
         public Ring(Point[] coords, double r1, double r2)
             : base(coords)
         {
-            this.radiusMin = r1;
-            this.radiusMax = r2;
+            this.RadiusMin = r1;
+            this.RadiusMax = r2;
         }
 
-        public double RadiusMin
-        {
-            get => this.radiusMin;
-            set
-            {
-                this.radiusMin = value;
-            }
-        }
+        public double RadiusMin { get; set; }
 
-        public double RadiusMax
-        {
-            get => this.radiusMax;
-            set
-            {
-                this.radiusMax = value;
-            }
-        }
+        public double RadiusMax { get; set; }
 
-        public double Square() => Math.PI * (this.radiusMax * this.radiusMax - this.radiusMin * this.radiusMin);
+        public double Area() => Math.PI * (this.RadiusMax * this.RadiusMax - this.RadiusMin * this.RadiusMin);
 
-        public double LengthMin() => 2 * Math.PI * this.radiusMin;
+        public double LengthMin() => 2 * Math.PI * this.RadiusMin; // Длина меньшей окружности
 
-        public double LengthMax() => 2 * Math.PI * this.radiusMax;
+        public double LengthMax() => 2 * Math.PI * this.RadiusMax; // Длина большей окружности
     }
 
-    public class Triangle : Shape
+    /// <summary>
+    /// Многоугольник создан для добавления к Shape новых методов для дальнешего удобства. 
+    /// abstract, так как не записан универсальный подсчет площади и периметра многоугольника
+    /// </summary>
+    public abstract class Polygon : Shape
+    {
+        public Polygon(int count)
+            : base(count) { }
+
+        public Polygon(Point[] coords)
+            : base(coords) { }
+
+        protected double LengthOfSide(Point p1, Point p2) => Math.Sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+
+        public virtual double Area() => 0;
+
+        public virtual double Perimetr() => 0;
+    }
+
+    /// <summary>
+    /// Треугольник
+    /// </summary>
+
+    public class Triangle : Polygon
     {
         public Triangle()
             : base(3) { }
@@ -143,25 +126,16 @@ namespace Task_3
         public Triangle(Point[] coords)
             : base(coords) { }
 
-        public double Square()
-        {
-            double sqr = Math.Abs((this.Coordinates[0].x - this.Coordinates[2].x) * (this.Coordinates[1].y - this.Coordinates[2].y)
+        public override double Area() => Math.Abs((this.Coordinates[0].x - this.Coordinates[2].x) * (this.Coordinates[1].y - this.Coordinates[2].y)
                 - (this.Coordinates[0].y - this.Coordinates[2].y) * (this.Coordinates[1].x - this.Coordinates[2].x)) / 2;
-            return sqr;
-        }
 
-        private double Length(Point p1, Point p2)
-        {
-            return Math.Sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-        }
-
-        public double Perimetr()
-        {
-            return Length(Coordinates[0], Coordinates[1])+ Length(Coordinates[0], Coordinates[2]) + Length(Coordinates[1], Coordinates[2]);
-        }
+        public override double Perimetr() => LengthOfSide(Coordinates[0], Coordinates[1]) + LengthOfSide(Coordinates[0], Coordinates[2]) + LengthOfSide(Coordinates[1], Coordinates[2]);
     }
 
-    class Rectangle : Shape
+    /// <summary>
+    /// Прямоугольник
+    /// </summary>
+    class Rectangle : Polygon
     {
         public Rectangle()
             : base(4) { }
@@ -169,11 +143,31 @@ namespace Task_3
         public Rectangle(Point[] coords)
             : base(coords) { }
 
-        public double Square()
-        {
-            var c = this.Coordinates;
-            return 1 / 2 * Math.Abs(c[0].x * c[1].y ); // доделать
-        }
+        private double Mult(int a, int b) => Coordinates[a].x * Coordinates[b].y; // просто для сокращения кода в Square
+
+        public override double Area() => 1 / 2 * Math.Abs(Mult(0, 1) + Mult(1, 2) + Mult(2, 3) + Mult(3, 0)
+                - Mult(1, 0) - Mult(2, 1) - Mult(3, 2) - Mult(0, 3)); // Методом Гаусса
+
+        public override double Perimetr() => LengthOfSide(Coordinates[0], Coordinates[1]) + LengthOfSide(Coordinates[1], Coordinates[2])
+            + LengthOfSide(Coordinates[2], Coordinates[3]) + LengthOfSide(Coordinates[3], Coordinates[0]);
+    }
+
+    /// <summary>
+    /// Квадрат
+    /// </summary>
+    public class Square : Polygon
+    {
+        public Square()
+            : base(4) { }
+
+        public Square(Point[] coords)
+            : base(coords) { }
+
+        private double Mult(int a, int b) => Coordinates[a].x * Coordinates[b].y; // просто для сокращения кода в Square
+
+        public override double Area() => LengthOfSide(Coordinates[0], Coordinates[1]) * LengthOfSide(Coordinates[0], Coordinates[1]);
+
+        public override double Perimetr() => 4 * LengthOfSide(Coordinates[0], Coordinates[1]);
     }
 
     class Program
