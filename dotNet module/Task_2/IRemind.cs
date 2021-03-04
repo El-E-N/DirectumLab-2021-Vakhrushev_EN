93f2d7b912c6d529137adcf67f4bdb3d1d9bab09
El-E-N <egor.v1309@gmail.com>
2021-03-04 20:17:06 +0400

    Исправил все, как было описано в PR

---
+++ dotNet module/Task_2/IRemind.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_2
+{
+    /// <summary>
+    /// интерфейс напоминания
+    /// </summary>
+    public interface IRemind
+    {
+        public DateTime EventTime { get; set; }
+    }
+}
