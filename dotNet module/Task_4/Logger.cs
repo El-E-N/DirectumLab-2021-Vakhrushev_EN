c0ee620cb9605a88553b869bd6b377678a1b4061
El-E-N <egor.v1309@gmail.com>
2021-03-08 14:06:42 +0400

    Выполнил 4 задание

---
+++ dotNet module/Task_4/Logger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Task_4
+{
+    /// <summary>
+    /// Логгер - класс для ведения логов.
+    /// </summary>
+    public class Logger : IDisposable
+    {
+        /// <summary>
+        /// Освобождение памяти для последующего использования файлов логов в другой программе
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.logWriter != null)
+                    this.logWriter.Close();
+                if (this.logFile != null)
+                    this.logFile.Close();
+            }
+        }
+
+        /// <summary>
+        /// Файл логов.
+        /// </summary>
+        private FileStream logFile;
+
+        /// <summary>
+        /// Писатель в лог.
+        /// </summary>
+        private StreamWriter logWriter;
+
+        /// <summary>
+        /// Создать объект.
+        /// </summary>
+        /// <param name="fileName">Имя файла логов.</param>
+        public Logger(string fileName)
+        {
+            this.logFile = new FileStream(fileName, FileMode.Append);
+            this.logWriter = new StreamWriter(this.logFile);
+        }
+
+        public void WriteString(string data)
+        {
+            this.logWriter.WriteLine(data);
+        }
+    }
+}
---
+++ dotNet module/Task_4/MeetingWithTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Task_2;
+
+namespace Task_4
+{
+    /// <summary>
+    /// Добавлен тип
+    /// </summary>
+    public class MeetingWithTypes : Meeting
+    {
+        private string type = string.Empty;
+        private static string[] possibleEvents = { "совещание", "поручение", "звонок", "день рождения" };
+
+        public string Type 
+        {
+            get => this.type;
+            set
+            {
+                if (Array.IndexOf(possibleEvents, value) > -1)
+                    this.type = value;
+                else
+                    Console.WriteLine("Недопустимое значение для события!");
+            }
+        }
+    }
+}
