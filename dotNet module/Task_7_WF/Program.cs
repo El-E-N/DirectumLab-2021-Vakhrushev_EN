ef09f5a7a7fdb28dd1459f6fca559a5d298043c4
El-E-N <egor.v1309@gmail.com>
2021-03-16 20:10:15 +0400

    Исправил недочеты

--- dotNet module/Task_7_WF/Program.cs
+++ dotNet module/Task_7_WF/Program.cs
@@ -7,30 +7,28 @@ namespace Task_7_WF
 {
     public static class Program
     {
+        public static string RtfReader(string gzipFile)
+        {
+            using (var sourceStream = new FileStream(gzipFile, FileMode.OpenOrCreate))
+            {
+                using (var decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                {
+                    using (var myFile = new StreamReader(decompressionStream))
+                    {
+                        return myFile.ReadToEnd();
+                    }
+                }
+            }
+        }
+
         [STAThread]
         public static void Main()
         {
             var gzipFile = "q2.rtf.gz";
-            var resultFile = "myFile.txt";
-            var infoOfFile = string.Empty;
-            using (var sourceStream = new FileStream(gzipFile, FileMode.OpenOrCreate))
-            {
-                using (var targetStream = File.Create(resultFile))
-                {
-                    using (var decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
-                    {
-                        decompressionStream.CopyTo(targetStream);
-                    }
-                }
-                using (var myFile = new StreamReader(resultFile))
-                {
-                    infoOfFile = myFile.ReadToEnd();
-                }
-            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             var form1 = new Form1();
-            form1.richTextBox1.Rtf = infoOfFile;
+            form1.richTextBox1.Rtf = RtfReader(gzipFile);
             Application.Run(form1);
         }
     }
