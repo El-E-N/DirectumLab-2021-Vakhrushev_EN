4c1a155297634e70b28b135be9a53eaea945c0f4
El-E-N <egor.v1309@gmail.com>
2021-03-21 18:32:48 +0400

    Создал 12 задание

---
+++ dotNet module/Task_12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace Task_12
+{
+    public class Program
+    {
+        public static void ObjectPropertyInfo(Type type)
+        {
+            foreach (var property in type.GetProperties())
+            {
+                Console.WriteLine($"{property.PropertyType} {property.Name}");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var myType = Type.GetType("Task_12.User", false, true);
+            ObjectPropertyInfo(myType);
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public User(string n, int a)
+        {
+            Name = n;
+            Age = a;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Имя: {Name}  Возраст: {Age}");
+        }
+        public int Payment(int hours, int perhour)
+        {
+            return hours * perhour;
+        }
+    }
+}
