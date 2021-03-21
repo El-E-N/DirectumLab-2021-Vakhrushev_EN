ba2de75e7e5bfea5108ce541b700298d4d334cb6
El-E-N <egor.v1309@gmail.com>
2021-03-22 00:24:39 +0400

    Здесь сделано 4 задание

---
+++ dotNet module/Task_12_Framework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_12
+{
+    /// <summary>
+    /// Класс выполнения программы.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Основной метод программы.
+        /// </summary>
+        /// <param name="args">Для консоли.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 4:");
+            foreach (var pair in ConfigReader.GetPairs())
+                Console.WriteLine("Key: " + pair["Key"] + ", Value: " + pair["Value"]);
+            Console.ReadLine();
+        }
+    }
+}
