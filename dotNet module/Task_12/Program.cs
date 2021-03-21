9723406d04aba5d6f41db4c0596d6267b0f1a542
El-E-N <egor.v1309@gmail.com>
2021-03-22 00:23:27 +0400

    Здесь сделаны 1, 2, 3 задания

--- dotNet module/Task_12/Program.cs
+++ dotNet module/Task_12/Program.cs
@@ -1,41 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Task_12
 {
+    /// <summary>
+    /// Класс выполнения программы.
+    /// </summary>
     public class Program
     {
-        public static void ObjectPropertyInfo(Type type)
+        /// <summary>
+        /// Метод для 1 задания.
+        /// </summary>
+        /// <param name="obj">Объект.</param>
+        /// <returns>Названия и значения свойств объекта.</returns>
+        public static List<Dictionary<string, string>> ObjectPropertyInfo(object obj)
         {
-            foreach (var property in type.GetProperties())
-            {
-                Console.WriteLine($"{property.PropertyType} {property.Name}");
-            }
+            var listResult = new List<Dictionary<string, string>>();
+            var type = obj.GetType();
+            foreach (var property in type.GetProperties(BindingFlags.DeclaredOnly
+                            | BindingFlags.Instance | BindingFlags.NonPublic
+                            | BindingFlags.Static | BindingFlags.Public))
+                listResult.Add(new Dictionary<string, string> 
+                { 
+                    { "Name", property.Name }, 
+                    { "Value", property.GetValue(obj).ToString() } 
+                });
+            return listResult;
         }
 
+        /// <summary>
+        /// Метод для 2 задания. Выводит в консоль названия и значения свойств объекта класса по умолчанию.
+        /// </summary>
+        /// <param name="pathOfAssembly">Путь к решению (.dll).</param>
+        /// <param name="pathOfClass">Путь к классу.</param>
+        public static void ShowPropertyInfo(string pathOfAssembly, string pathOfClass)
+        {
+            var assembly = Assembly.LoadFrom(pathOfAssembly);
+            var type = assembly.GetType(pathOfClass, true, true);
+            var obj = Activator.CreateInstance(type);
+            foreach (var property in type.GetProperties(BindingFlags.DeclaredOnly
+                            | BindingFlags.Instance | BindingFlags.NonPublic
+                            | BindingFlags.Static | BindingFlags.Public))
+                Console.WriteLine($"Имя свойства: {property.Name}; значение: {property.GetValue(obj)}");
+        }
+
+        /// <summary>
+        /// Метод для 3 задания.
+        /// </summary>
+        /// <param name="obj">Объект.</param>
+        /// <returns>Названия и значения свойств объекта.</returns>
+        public static List<Dictionary<string, string>> ObjectPropertyInfoWithoutObsolete(object obj)
+        {
+            var listResult = new List<Dictionary<string, string>>();
+            var type = obj.GetType();
+            foreach (var property in type.GetProperties(BindingFlags.DeclaredOnly
+                            | BindingFlags.Instance | BindingFlags.NonPublic
+                            | BindingFlags.Static | BindingFlags.Public))
+            {
+                bool propertyHasObsolete() 
+                {
+                    foreach (ObsoleteAttribute a in property.GetCustomAttributes())
+                        return true;
+                    return false;
+                }
+
+                if (!propertyHasObsolete())
+                    listResult.Add(new Dictionary<string, string>
+                    {
+                        { "Name", property.Name },
+                        { "Value", property.GetValue(obj).ToString() }
+                    });
+            }
+
+            return listResult;
+        }
+
+        /// <summary>
+        /// Основной метод программы.
+        /// </summary>
+        /// <param name="args">Для консоли.</param>
+        [Obsolete]
         public static void Main(string[] args)
         {
-            var myType = Type.GetType("Task_12.User", false, true);
-            ObjectPropertyInfo(myType);
-        }
-    }
+            Console.WriteLine("Задание 1:");
+            foreach (var dict in ObjectPropertyInfo(new User("MyName", 22, 12345)))
+                Console.WriteLine($"Имя свойства: {dict["Name"]}; значение: {dict["Value"]}");
+            
+            Console.WriteLine("\nЗадание 2:");
+            var pathOfAssembly = "Task_12.dll";
+            var pathOfClass = "Task_12.User";
+            ShowPropertyInfo(pathOfAssembly, pathOfClass);
 
-    public class User
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public User(string n, int a)
-        {
-            Name = n;
-            Age = a;
-        }
-        public void Display()
-        {
-            Console.WriteLine($"Имя: {Name}  Возраст: {Age}");
-        }
-        public int Payment(int hours, int perhour)
-        {
-            return hours * perhour;
+            Console.WriteLine("\nЗадание 3:");
+            foreach (var dict in ObjectPropertyInfoWithoutObsolete(new User("MyName", 22, 12345)))
+                Console.WriteLine($"Имя свойства: {dict["Name"]}; значение: {dict["Value"]}");
         }
     }
 }
---
+++ dotNet module/Task_12/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task_12
+{
+    /// <summary>
+    /// Тестовый класс.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public static string Name { get; set; }
+
+        /// <summary>
+        /// Возраст.
+        /// </summary>
+        private int Age { get; set; }
+
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        [Obsolete]
+        public double ID { get; }
+
+        /// <summary>
+        /// Конструктор с параметрами.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <param name="age">Возраст.</param>
+        /// <param name="id">Идентификатор.</param>
+        [Obsolete]
+        public User(string name, int age, int id)
+        {
+            Name = name;
+            this.Age = age;
+            this.ID = id;
+        }
+
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        [Obsolete]
+        public User()
+        {
+            this.Age = 1;
+            this.ID = 1;
+        }
+    }
+}
