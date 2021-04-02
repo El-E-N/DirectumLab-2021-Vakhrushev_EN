0fa8eddb880278c2c6de5758d445534bda4fa65f
El-E-N <egor.v1309@gmail.com>
2021-04-02 12:15:56 +0400

    Добавлен контроллер карт

---
+++ server/PlanPoker/Controllers/CardController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlanPoker.DTO;
+using PlanPoker.DTO.Builders;
+using PlanPoker.Services;
+using System.Linq;
+
+namespace PlanPoker.Controllers
+{
+    /// <summary>
+    /// Контроллер карт.
+    /// </summary>
+    [ApiController]
+    [Route("/api/[controller]/[action]")]
+    public class CardController
+    {
+        /// <summary>
+        /// Сервисы карт.
+        /// </summary>
+        private readonly CardService service;
+
+        /// <summary>
+        /// Конструктор игрока.
+        /// </summary>
+        /// <param name="service">Сервисы.</param>
+        public CardController(CardService service)
+        {
+            this.service = service;
+        }
+
+        /// <summary>
+        /// Получение всех оценок.
+        /// </summary>
+        /// <returns>Все оценки.</returns>
+        public IQueryable<CardDTO> GetCards()
+        {
+            return this.service.GetCards()
+                .Select(card => CardDTOBuilder.Build(card));
+        }
+    }
+}
